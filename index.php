22af6d4d99c3679278a7da412dc24013bcc54696
Marten <marten.pipenberg@gmail.com>
2023-11-14 11:28:54 +0200

    Add basic blog view

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 </head>
+
 <body>
-    <h1>Hello wordpress :D</h1>
+    <div class="container">
+        <div class="content">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : ?>
+                    <?php the_post(); ?>
+                    <div class="card mb-2">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                <?php the_title(); ?>
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
 </body>
+
 </html>
